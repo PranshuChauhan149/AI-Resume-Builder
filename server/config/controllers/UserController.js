f429e74ab9424512287d91c6217fe762413a4a1f
PranshuChauhan149 <pranshuchauhan149@gmail.com>
2025-11-13 23:37:13 +0530

    writing the register Route

--- server/config/controllers/UserController.js
+++ server/config/controllers/UserController.js
@@ -1,8 +1,8 @@
 import User from "../../models/User";
 import jwt from "jsonwebtoken";
-
+import bcrypt from "bcrypt";
 const generateToken = (userId) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRECT, {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
   return token;
@@ -17,5 +17,44 @@ export const registerUser = async (req, res) => {
     if (user) {
       return res.status(400).json({ message: "User Already exists" });
     }
-  } catch (error) {}
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashPassword,
+    });
+    const token = generateToken(newUser._id);
+    newUser.password = undefined;
+    return res
+      .status(201)
+      .json({ message: "User created successflly", token, user: newUser });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
 };
+
+
+export const LoginUser = async (req, res) => {
+  try {
+    const {  email, password } = req.body;
+ 
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid" });
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashPassword,
+    });
+    const token = generateToken(newUser._id);
+    newUser.password = undefined;
+    return res
+      .status(201)
+      .json({ message: "User created successflly", token, user: newUser });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
