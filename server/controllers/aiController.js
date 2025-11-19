f4b0ee1c2c4558b3851aaf2d0d3c66e4819f1569
PranshuChauhan149 <pranshuchauhan149@gmail.com>
2025-11-19 11:08:24 +0530

    adding ai echancement

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -1,5 +1,28 @@
+import OpenAI from "openai";
+const enhanceProfessionalSummary = async (req, res) => {
+  try {
+    const { userContent } = req.body;
+    if (!userContent) {
+      return res.status(400).json({ message: "missing content" });
+    }
 
-const Ehance = async(req,res)=>{
-  const {summary} = req.body;
-  
-}
+    const response = await ai.chat.completions.create({
+      model: process.env.OPENAI_MODEL,
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are an expert in resume writing. Your task is to enhance the professional summry of a resume. The summry should be 1-2 sentences also highlighting key skills, experience, and career objectives. Make it compelling and ATS-friendly. and only return text no options or anything else.",
+        },
+        {
+          role: "user",
+          content: userContent,
+        },
+      ],
+    });
+    const enhancedContent = response.choices[0].message.content;
+    return res.status(200).json({ enhancedContent });
+  } catch (error) {
+    return res.status(401).json({ message: error.message });
+  }
+};
